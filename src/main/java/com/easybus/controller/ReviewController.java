d207d708a9aaa994cc48b20839180e5c6868261e
Bharath <varamasalva8@gmail.com>
2025-09-09 01:44:36 +0530

    Changes checking

--- src/main/java/com/easybus/controller/ReviewController.java
+++ src/main/java/com/easybus/controller/ReviewController.java
@@ -20,7 +20,6 @@ import com.easybus.model.ReviewRequest;
 import com.easybus.model.ReviewResponse;
 import com.easybus.service.ReviewService;
 
-
 @RestController
 @CrossOrigin(origins = "*")
 @RequestMapping("/api/reviews")
